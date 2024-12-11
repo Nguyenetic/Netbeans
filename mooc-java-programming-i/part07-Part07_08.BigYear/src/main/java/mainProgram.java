c518333d6fcfacf664e359456c85fa7d52828d7e
John Nguyen <john.nguyen9727@gmail.com>
2024-12-11 12:33:27 -0600

    Program that implements a database base on these commands:
        Add - adds a bird
    
        Observation - adds an observation
    
        All - prints all birds
    
        One - prints one bird
    
        Quit - ends the program

--- mooc-java-programming-i/part07-Part07_08.BigYear/src/main/java/mainProgram.java
+++ mooc-java-programming-i/part07-Part07_08.BigYear/src/main/java/mainProgram.java
@@ -1,15 +1,40 @@
 
 import java.util.Scanner;
 
+import java.util.Scanner;
+
 public class mainProgram {
-
     public static void main(String[] args) {
-        // NB! Do not create other scanner objects than the one below
-        // if and when you create other classes, pass the scanner to them
-        // as a parameter
-
         Scanner scan = new Scanner(System.in);
+        BirdDatabase database = new BirdDatabase();
 
+        while (true) {
+            System.out.println("? ");
+            String command = scan.nextLine();
+
+            if (command.equalsIgnoreCase("Quit")) {
+                break;
+            } else if (command.equalsIgnoreCase("Add")) {
+                System.out.println("Name: ");
+                String name = scan.nextLine();
+                System.out.println("Name in Latin: ");
+                String latinName = scan.nextLine();
+                database.addBird(name, latinName);
+            } else if (command.equalsIgnoreCase("Observation")) {
+                System.out.println("Bird? ");
+                String name = scan.nextLine();
+                if (!database.addObservation(name)) {
+                    System.out.println("Not a bird!");
+                }
+            } else if (command.equalsIgnoreCase("All")) {
+                database.printAll();
+            } else if (command.equalsIgnoreCase("One")) {
+                System.out.println("Bird? ");
+                String name = scan.nextLine();
+                database.printOne(name);
+            } else {
+                System.out.println("Unknown command");
+            }
+        }
     }
-
 }
