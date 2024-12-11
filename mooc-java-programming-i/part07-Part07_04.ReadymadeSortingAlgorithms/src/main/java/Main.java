b8517f7adf57ebd060124b15e38919df1f2b691f
John Nguyen <john.nguyen9727@gmail.com>
2024-12-11 10:50:12 -0600

    Started with first element then loop through the array list using length to iterate through the whole thing once the value is found.
    Second method to find the smallest index of the array list instead.
    
    Third method to find a small value after a certain one.
    
    Fourth method to store the value at a temp variable and reassign to "swap" the values.

--- mooc-java-programming-i/part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
+++ mooc-java-programming-i/part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
@@ -1,14 +1,23 @@
 
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
         // insert test code here
-        int[] array = {3, 1, 5, 99, 3, 12};
-        
-     System.out.println("Index of the smallest number: " + indexOfSmallest(array));
+        int[] numbers = {3, 1, 5, 99, 3, 12};
+
+        System.out.println("Index of the smallest number: " + indexOfSmallest(numbers));
+
+        System.out.println(Arrays.toString(numbers));
+
+        Main.swap(numbers, 1, 0);
+        System.out.println(Arrays.toString(numbers));
+
+        Main.swap(numbers, 0, 3);
+        System.out.println(Arrays.toString(numbers));
     }
-    
+
     public static int smallest(int[] array) {
         int smallest = array[0]; // Assume the first element is the smallest
         for (int number : array) {
@@ -18,20 +27,18 @@ public class Main {
         }
         return smallest;
     }
-    
+
     public static int indexOfSmallest(int[] array) {
         int index = 0; // Start with the first element as the smallest
-        for (int i = 1; i < array.length; i++){
-        if (array[i] < array[index]) {
-            index = i; // Loop to iterate through the list to find the smallest value
-                       // and update once found
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[index]) {
+                index = i; // Loop to iterate through the list to find the smallest value
+                // and update once found
+            }
         }
-    }
         return index;
     }
-    
 
-  
     public static int indexOfSmallestFrom(int[] table, int startIndex) {
         int index = startIndex; // Start searching from the provided index
         for (int i = startIndex + 1; i < table.length; i++) {
@@ -41,8 +48,13 @@ public class Main {
         }
         return index;
     }
+
+    public static void swap(int[] array, int index1, int index2) {
+
+        int temp = array[index1]; // Store the value at index1 in a temporary variable
+
+        array[index1] = array[index2]; // Assign the value at index2 to index1
+
+        array[index2] = temp;  // Assign the value in temp (original index1 value) to index2
+    }
 }
-    
-    
-
-
