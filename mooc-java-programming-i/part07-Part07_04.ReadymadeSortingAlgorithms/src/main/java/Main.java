60617215d261b1ab0d55a83b9f4ef54295e1fcfd
John Nguyen <john.nguyen9727@gmail.com>
2024-12-11 10:13:03 -0600

    Started with first element then loop through the array list using length to iterate through the whole thing once the value is found.

--- mooc-java-programming-i/part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
+++ mooc-java-programming-i/part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
@@ -5,6 +5,19 @@ public class Main {
     public static void main(String[] args) {
         // insert test code here
         int[] array = {3, 1, 5, 99, 3, 12};
+        
+     System.out.println("Index of the smallest number: " + indexOfSmallest(array));
+    }
+    
+    public static int indexOfSmallest(int[] array) {
+        int index = 0; // Start with the first element as the smallest
+        for (int i = 1; i < array.length; i++){
+        if (array[i] < array[index]) {
+            index = i; // Loop to iterate through the list to find the smallest value
+                       // and update once found
+        }
+    }
+        return index;
     }
 
 }
